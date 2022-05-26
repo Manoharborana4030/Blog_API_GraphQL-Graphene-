7279a5c021a2b57a5b6005a3f54547501195a57d
Manoharborana4030 <19mca050@nirmauni.ac.in>
2022-05-26 19:12:10 +0530

    create post done

--- Blog_API_Revised/API/models.py
+++ Blog_API_Revised/API/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200,default='My_bloggg')
+    author=models.ForeignKey(User, on_delete= models.CASCADE)
+    body=models.TextField()
+    post_date=models.DateField(auto_now_add=True)
+    likes=models.ManyToManyField(User,related_name='blog_posts')
+    header_image=models.ImageField(null=True,blank=True,upload_to="images/")
+
+    def total_likes(self):
+        return self.likes.count()
+    def __str__(self) -> str:
+        return self.title + '  |  '+str(self.author)
+
+    def get_absolute_url(self):
+        #return reverse("article-details", args=(str(self.id)))
+        return reverse('home')
+    
+class Comments(models.Model):
+    post=models.ForeignKey(Post,related_name='comments', on_delete= models.CASCADE)
+    name=models.CharField(max_length=255)
+    body=models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return '%s - %s' %(self.post.title,self.name)
--- Blog_API_Revised/API/schema.py
+++ Blog_API_Revised/API/schema.py
@@ -1,4 +1,4 @@
-from tkinter.tix import Tree
+from .models import Post,Comments
 from django.contrib.auth import get_user_model
 import graphene
 from graphene_django import DjangoObjectType
@@ -8,6 +8,18 @@ class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
 
+class PostType(DjangoObjectType):
+    class Meta:
+        model=Post
+        fields = "__all__"
+
+class PostInput(graphene.InputObjectType):
+    id=graphene.ID()
+    title=graphene.String()
+    tag=graphene.String()
+    body=graphene.String()
+    post_date=graphene.String()
+    header_image=graphene.String()
 
 class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
@@ -31,6 +43,21 @@ class CreateUser(graphene.Mutation):
 
         return CreateUser(user=user)
 
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        post_data=PostInput(required=True)
+
+    post=graphene.Field(PostType)
+    
+    def mutate(root, info, post_data=None):
+        post=Post(title=post_data.title,
+        tag=post_data.tag,
+        body=post_data.body,
+        post_date=post_data.post_date,
+        header_image=post_data.header_image
+        )
+        post.save()
+        return CreatePost(post=post_data)
 
 
 class Query(graphene.ObjectType):
@@ -51,6 +78,7 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_post=CreatePost.Field()
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
